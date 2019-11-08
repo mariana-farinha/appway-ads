fe3bd97539887734da09c749f82ab8026adbd69c
Mariana <mary.farinha@gmail.com>
2019-11-08 15:10:30 +0100

    fix: use className instead of classList due to IE lack of support

--- onboarding-for-wealth/300x250/main.js
+++ onboarding-for-wealth/300x250/main.js
@@ -2,9 +2,9 @@ window.addEventListener("load", function() {
   var ad = (function() {
     var CONST = {
       CSS_CLASS: {
-        SHOW: "show",
-        HIDE: "hide",
-        SHOW_BACKGROUND: "show-background"
+        SHOW: " show ",
+        HIDE: " hide ",
+        SHOW_BACKGROUND: " show-background "
       }
     };
     var adContainer = document.getElementById("aw_ad");
@@ -12,20 +12,20 @@ window.addEventListener("load", function() {
     var slides = document.getElementsByClassName("aw_slide");
 
     var init = function() {
-      logo.classList.add(CONST.CSS_CLASS.SHOW);
+      logo.className += CONST.CSS_CLASS.SHOW;
     };
 
     var showSlide = function(index) {
       setTimeout(function() {
         index > 0
-          ? slides[index - 1].classList.replace(
+          ? (slides[index - 1].className = slides[index - 1].className.replace(
               CONST.CSS_CLASS.SHOW,
               CONST.CSS_CLASS.HIDE
-            )
+            ))
           : null;
-        slides[index].classList.add(CONST.CSS_CLASS.SHOW);
+        slides[index].className += CONST.CSS_CLASS.SHOW;
         if (index === slides.length - 1) {
-          adContainer.classList.add(CONST.CSS_CLASS.SHOW_BACKGROUND);
+          adContainer.className += CONST.CSS_CLASS.SHOW_BACKGROUND;
         }
       }, 2500 * index + 1000);
     };
